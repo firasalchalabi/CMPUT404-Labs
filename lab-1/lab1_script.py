b4ba52382c76e9bfcad7e99cd7094964600d5521
Firas Al Chalabi <firas_alchalabi@hotmail.com>
2022-09-14 18:32:10 -0600

    Pushing Python script

--- lab-1/lab1_script.py
+++ lab-1/lab1_script.py
@@ -3,4 +3,9 @@ import requests
 print(requests.__version__)
 
 google_homepage = requests.get("http://www.google.com")
-print(google_homepage.status_code)
+print(google_homepage.content)
+
+print("=======================================================================")
+
+python_script_file = requests.get("https://github.com/firasalchalabi/CMPUT404-Labs/blob/main/lab-1/lab1_script.py")
+print(python_script_file.text)
